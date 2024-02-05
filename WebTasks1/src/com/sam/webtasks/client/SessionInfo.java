c6d26f999e882f180acd03663816f962508bd310
Sam Gilbert <mail@samgilbert.net>
2024-02-05 12:15:40 +0000

    version 2, first release candidate

--- WebTasks1/src/com/sam/webtasks/client/SessionInfo.java
+++ WebTasks1/src/com/sam/webtasks/client/SessionInfo.java
@@ -13,10 +13,10 @@ public class SessionInfo {
 	public static int experimentType = Names.EXPERIMENT_PROLIFIC;
 	
 	//what is the name for this experiment?
-	public static String experimentCode="GC3";
+	public static String experimentCode="GC3"; 
 	
 	//which version of the experiment is this?
-	public static int experimentVersion=1;  
+	public static int experimentVersion=2;  
 	
 	//what is the minimum permitted screen size in pixels?
 	//if the screen is smaller than this the participant will be asked
